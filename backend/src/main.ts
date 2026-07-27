e91ebd6c4d0b91b7c140e0253da0205103c42b44
Atigbi Emmanuel Ayomiku <e.atigbi@alustudent.com>
2026-07-27 22:59:57 +0200

    Trigger Railway rebuild

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -17,7 +17,7 @@ async function bootstrap() {
     transform: true,
   }));
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 8080);
 }
 
 bootstrap().catch((err) => {
